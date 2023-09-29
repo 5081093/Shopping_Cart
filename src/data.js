d7fa540daa35d3e0490f2c5d65251fefb5606d59
Amitesh Kumar Singh <as5081093@gmail.com>
2023-09-29 12:36:02 +0530

    Project Completed

---
+++ src/data.js
@@ -0,0 +1,41 @@
+let shopItemsData = [
+    {
+      id: "abc",
+      name: "White  T-shirt",
+      price: 45,
+      desc: "Cotton T- shirt",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "def",
+      name: "Office Shirt",
+      price: 65,
+      desc: "Office PAtterns For Men",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "ghi",
+      name: "Hoodie",
+      price: 25,
+      desc: "Trending Men Hoodie",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "jkl",
+      name: "Shirt",
+      price: 35,
+      desc: "Regular Fit Shirts",
+      img: "images/img-4.jpg",
+    },
+
+
+{
+id: "jklm",
+name: "Jacket",
+price: 35,
+desc: "Trending Collection",
+img: "images/img-8.jpg",
+},
+
+
+  ];
